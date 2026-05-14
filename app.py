a5b054574133274afaa4cb351cc24bd1e47dc4eb
ardatunca1925-commits <arda.tunca1925@gmail.com>
2026-05-14 21:12:50 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="EKG Dashboard", layout="wide")
+
+st.title("❤️ Akıllı EKG İzleme Sistemi")
+
+st.subheader("Canlı EKG Takibi")
+
+st.line_chart([100,120,110,130,125,140,135,150])
